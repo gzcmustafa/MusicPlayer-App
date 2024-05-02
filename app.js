e24bb860263b9b671d3e8c3c5f00067d095c9886
Mustafa Gözcü <gozcumustafa34@gmail.com>
2024-05-02 20:14:27 +0300

    refactor(design): Optimized design structures

--- app.js
+++ app.js
@@ -53,12 +53,12 @@ next.addEventListener("click",()=>{
 
 function pauseMusic(){
     container.classList.remove("playing")
-    play.classList = "fa-solid fa-play"
+    play.querySelector("i").classList = "fa-solid fa-play"
     audio.pause();
 }
 function playMusic() {
     container.classList.add("playing");
-    play.classList = "fa-solid fa-pause";
+    play.querySelector("i").classList = "fa-solid fa-pause";
     audio.play()
 }
 
